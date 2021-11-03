9966d316516ab2317880456ab289f890a64d6ee1
ljliu <45717107+ljliuX@users.noreply.github.com>
2021-11-03 17:27:44 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,11 @@
 
 git clone --recursive https://github.com/ceph/ceph.git
 cd ceph
+mkdir -p /opt/ceph
 ./install-deps.sh
-./do_cmake.sh -DCMAKE_INSTALL_PREFIX=/usr/local -DWITH_RDMA=ON
+./do_cmake.sh -DCMAKE_INSTALL_PREFIX=/opt/ceph -DWITH_RDMA=ON
+apt-get install -y ninja
 cd build
 ninja
 sudo ninja install
-tar zcvf ceph-rdma.tar.gz /usr/local
+tar zcvf ceph-rdma.tar.gz /opt/ceph
